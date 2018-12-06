1ed16e7898d4a0f4264ab230c9a4d0c6002b056e
Muna <mmhussein125@gmail.com>
2018-12-06 14:53:01 +0000

    Moves handleClick method from Board to Game

--- src/index.js
+++ src/index.js
@@ -12,18 +12,6 @@ function Square(props) {
 
 class Board extends React.Component {
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   restartGame(event) {
     this.state = {
       squares: Array(9).fill(null),
@@ -77,6 +65,23 @@ class Game extends React.Component {
       xIsNext: true,
     };
   }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
