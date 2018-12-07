3498890a3e7ac1f708dcb6861156b933feb67eee
Muna <mmhussein125@gmail.com>
2018-12-07 23:58:22 +0000

    Imports enzyme to test files

--- src/Components/Board/board.test.js
+++ src/Components/Board/board.test.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Board from './board'
 import {shallow, mount} from 'enzyme'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   let squares = Array(9).fill(null)
